b0dcb51da244d96d8fa7070e3d39110aff94fc56
anamestre <anamestree@gmail.com>
2019-10-14 23:17:23 +0200

    2nd exercise: implementation for PoS part

--- first-graded-assignment/python_advanced/exercise2b_word-similarity/exercise2b_word_similarity.py
+++ first-graded-assignment/python_advanced/exercise2b_word-similarity/exercise2b_word_similarity.py
@@ -17,15 +17,26 @@ from scipy.stats import spearmanr
 
 def load_sim_benchmark(fname="SimLex-999/SimLex-999.txt"):
     graded_similarity = {}
+    sim_a = {}
+    sim_n = {}
+    sim_v = {}
+    #graded_sim_pos = {}
     with open(fname) as f:
     # the first line contains the column names, we'll skip it
         f.readline()
         # iterates through each line in the reference file (see exercise1)
         # word1	word2	POS	SimLex999	conc(w1)	conc(w2)	concQ	Assoc(USF)	SimAssoc333	SD(SimLex)
         for line in f.readlines():
-            word1, word2, _, simLex999, _, _, _, _, _, _ = line.strip().split("\t")
+            word1, word2, pos, simLex999, _, _, _, _, _, _ = line.strip().split("\t")
             graded_similarity[(word1, word2)] = simLex999
-        return graded_similarity
+            if pos == 'A':
+                sim_a[(word1, word2)] = simLex999
+            if pos == 'N':
+                sim_n[(word1, word2)] = simLex999
+            if pos == 'V':
+                sim_v[(word1, word2)] = simLex999
+            #graded_sim_pos[pos][(word1, word2)] = simLex999
+        return graded_similarity, sim_a, sim_n, sim_v
 
 def load_wordpairs(fname="SimLex-999/SimLex-999.txt"):
     with open(fname) as f:
@@ -69,8 +80,7 @@ def print_similarity_predictions(word_pairs, model_predictions, gold_ratings, ou
         file.write("Word1 \t Word2 \t Model prediction \t Gold rating")
         for w1, w2 in word_pairs:
             file.write('\n')
-            file.write(w1 + "\t" + w2 + "\t" + str(model_predictions[(w1,w2)]) + "\t" + str(gold_ratings[(w1, w2)]))
-    
+            file.write(w1 + "\t" + w2 + "\t" + str(model_predictions[(w1,w2)]) + "\t" + str(gold_ratings[(w1, w2)][0]))
 
 if __name__=="__main__":
     benchmark_data_fname = "SimLex-999/SimLex-999.txt"
@@ -86,23 +96,30 @@ if __name__=="__main__":
         sem_model = load_model(word2vec_modelfname, is_binary=True)    
     
     # TODO: Load the word similarity benchmark
-    graded_sim = load_sim_benchmark()
+    graded_sim, simA, simN, simV = load_sim_benchmark()
     
     # TODO: Estimate the similarity of all word pairs with the model 
     # ==> 
     word_pairs = load_wordpairs()
     predicted_sim = get_similarity_predictions(sem_model, word_pairs)
+    pred_A = {x:predicted_sim[x] for x in simA.keys()}
+    pred_N = {x:predicted_sim[x] for x in simN.keys()}
+    pred_V = {x:predicted_sim[x] for x in simV.keys()}
     # print(sim)
     
     # TODO: Print the model predictions and the human judgements / write them into a file
     print_similarity_predictions(word_pairs, predicted_sim, graded_sim)
     
-    # TODO: Evaluate the model against the gold ratings using by correlating the scores
+    # TODO: Evaluate the model against the gold ratings by correlating the scores
     # using Spearman's rho
     # ==> evaluate()
-    predicts = list(predicted_sim.values())
-    golds = list(graded_sim.values())
-    print(evaluate("Spearman", predicts, golds))
-    
+    #predicts = list(predicted_sim.values()[0])
+    print("Spearman Results for the whole dataset:")
+    print(evaluate("Spearman", list(predicted_sim.values()), list(graded_sim.values())))
+    print("Spearman Results for PoS = A:")
+    print(evaluate("Spearman", list(pred_A.values()), list(simA.values())))
+    print("Spearman Results for PoS = N:")
+    print(evaluate("Spearman", list(pred_N.values()), list(simN.values())))
+    print("Spearman Results for PoS = V:")
+    print(evaluate("Spearman", list(pred_V.values()), list(simV.values())))
     # TODO: See 3. Questions in the ex2b_instructions_word-similarity.txt
-    
